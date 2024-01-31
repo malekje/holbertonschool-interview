3e78d2500d77aaa6ae4ae459be4b6c988ae26f30
Malek Mayeh <31927278+malekje@users.noreply.github.com>
2024-01-31 07:53:41 +0000

    LL Cycle

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+
+/**
+ * check_cycle - check cycle in linked list
+ * @list: linked list
+ * Return: 0 if is a cycle else 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *lista;
+	listint_t *listb;
+
+	if (list == NULL)
+		return (0);
+
+	lista = list;
+	listb = list->next;
+
+	while (lista != NULL && listb != NULL && listb->next != NULL)
+	{
+		if (lista == listb)
+			return (1);
+		lista = lista->next;
+		listb = listb->next->next;
+	}
+	return (0);
+}
